5ab4a75f6da4fc2ab897ee4fecdb203a25f6403c
Pavani Ariga <pavaniariga1@gmail.com>
2026-02-03 22:22:46 +0530

    Create do_while_task.java

---
+++ do_while_task.java
@@ -0,0 +1,12 @@
+package looping_statements;
+
+public class do_while_task {
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println("This will print at least once");
+			i++;
+		} while(i<=1);
+	}
+
+}
